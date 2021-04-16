12beedf471c428bd15e2bb19cd01147a3764fc38
Mateusz Ożóg <mateusz2345678@gmail.com>
2021-04-16 10:16:32 +0200

    :+1: Footer fixes

--- learn-it-web/src/components/Footer.vue
+++ learn-it-web/src/components/Footer.vue
@@ -1,7 +1,18 @@
 <template>
 	<footer class="bg-dark text-light py-3">
 		<div class="container">
-			<p class="mb-0"><i class="bx bx-copyright"></i> {{ currentYear }} by Mati</p>
+			<div class="row">
+				<div class="col">
+					<p class="mb-0">
+						<i class="bx bx-meteor me-2"></i>Learn<span class="text-primary fw-bold">IT</span> by
+						<a href="https://github.com/Mvtthew/">Mati</a>
+					</p>
+				</div>
+				<div class="col text-end">
+					<i class="bx bx-copyright"></i>
+					{{ currentYear }}
+				</div>
+			</div>
 		</div>
 	</footer>
 </template>
