713570714d771fa7642d149914aab11c271a617d
Mateusz Ożóg <mateusz2345678@gmail.com>
2021-04-30 09:10:34 +0200

    :+1: Config update com

--- learn-it-server/config.js
+++ learn-it-server/config.js
@@ -1,4 +1,5 @@
 module.exports = {
+	// Your random string for jwt hash seed
 	JWT_SECRET:
 		"fZfSCaYzDQRNdCM5VSbxGfrwP6RrKwUNntHYrZ7BEwMdwfWjhyunf22C9fkbfstVBEnsBLRwmVyqyLJcxARUutexnJN4d6YRDhdQFwwR4PRt23fWw5SP3MKHjp9dsBqX",
 };
